565a4a17e697f82fcab79459e3cf5d867a2f680f
Henrique Pereira <henrique802011@gmail.com>
2022-11-29 21:27:14 -0300

    coffee delivery - confirm order page forms in progress

--- src/pages/completePedido/index.tsx
+++ src/pages/completePedido/index.tsx
@@ -1,14 +1,49 @@
-import {CompleteOrder, ConfirmOrderContainer, OrderCart} from './styles'
+import { MapPinLine } from "phosphor-react";
+import { defaultTheme } from "../../styles/themes/default";
+import {
+  CompleteOrder,
+  CompleteOrderContainer,
+  ConfirmOrderContainer,
+  InputForm,
+  OrderCart,
+} from "./styles";
 
 export function ConfirmOrder() {
   return (
     <ConfirmOrderContainer>
-      <CompleteOrder>
-        <h2>Complete seu pedido</h2>
-      </CompleteOrder>
+      <CompleteOrderContainer>
+        <h3>Complete seu pedido</h3>
+        <CompleteOrder>
+          <div className="titulo_complete_order">
+            <MapPinLine size={24} color={defaultTheme["yellow-600"]} />
+            <div className="chamada_titulo_complete_order">
+              <p>Endereço de Entrega</p>
+              <p>Informe o endereço onde deseja receber seu pedido</p>
+            </div>
+          </div>
+
+          <form>
+            <div className="inputContainer">
+              <InputForm type="text" placeholder="CEP" maxWidth={200} />
+            </div>
+            <div className="inputContainer">
+              <InputForm type="text" placeholder="Rua" />
+            </div>
+            <div className="inputContainer">
+              <InputForm type="text" placeholder="Número" maxWidth={200} />
+              <InputForm type="text" placeholder="Complemento" maxWidth={348} />
+            </div>
+            <div className="inputContainer">
+              <InputForm type="text" placeholder="Bairro" maxWidth={200} />
+              <InputForm type="text" placeholder="Cidade" maxWidth={276} />
+              <InputForm type="text" placeholder="UF" maxWidth={60} />
+            </div>
+          </form>
+        </CompleteOrder>
+      </CompleteOrderContainer>
       <OrderCart>
         <h2>Cafés selecionados</h2>
       </OrderCart>
     </ConfirmOrderContainer>
-  )
+  );
 }
--- src/pages/completePedido/styles.ts
+++ src/pages/completePedido/styles.ts
@@ -12,11 +12,72 @@ export const ConfirmOrderContainer = styled.section`
   gap: 1rem;
 `;
 
-export const CompleteOrder = styled.div`
+export const CompleteOrderContainer = styled.div`
   max-width: 640px;
   width: 100%;
-  background-color: ${(props) => props.theme["gray-200"]};
   color: ${(props) => props.theme["gray-900"]};
+
+  h3 {
+    margin-bottom: 1rem;
+  }
+`;
+
+export const CompleteOrder = styled.div`
+  padding:2.2rem;
+  border-radius: 8px;
+  background-color: ${(props) => props.theme["gray-200"]};
+
+  .titulo_complete_order {
+    display: flex;
+    align-items: center;
+    gap: 0.7rem;
+
+    .chamada_titulo_complete_order {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+
+      p:first-child {
+        color: ${(props) => props.theme["gray-800"]};
+        font-weight: 400;
+        font-size: 1.09rem;
+        line-height: 130%;
+        margin-bottom: 2px;
+      }
+
+      p:nth-child(2) {
+        color: ${(props) => props.theme["gray-700"]};
+        font-weight: 400;
+        font-size: 0.9rem;
+        line-height: 130%;
+      }
+    }
+  }
+  form {
+    margin-top:1rem;
+    display: flex;
+    flex-direction: column;
+    gap: 0.8rem;
+    .inputContainer {
+      width: 100%;
+      display: flex;
+      gap: 0.8rem;
+    }
+  }
+`;
+
+interface InputFormProps {
+  maxWidth?: number;
+}
+
+export const InputForm = styled.input<InputFormProps>`
+  width: ${(props) => (props.maxWidth ? props.maxWidth + "px" : "100%")};
+  height: 42px;
+  border-radius: 4px;
+  padding: 0.8rem;
+  background-color: ${(props) => props.theme["gray-400"]};
+  color: ${(props) => props.theme["gray-800"]};
+  border:0;
 `;
 
 export const OrderCart = styled.div`
