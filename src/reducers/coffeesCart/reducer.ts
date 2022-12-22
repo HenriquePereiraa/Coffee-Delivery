ea5f923ec236c74c2620407fcea47eb513206d85
Henrique Pereira <henrique802011@gmail.com>
2022-12-22 07:39:53 -0300

    coffee delivery - reducers created

--- src/reducers/coffeesCart/reducer.ts
+++ src/reducers/coffeesCart/reducer.ts
@@ -1,6 +1,42 @@
+import { ActionTypes } from "./actions";
+
 export interface CoffeeData {
   id: string;
   title: string;
   price: number;
   amount: number;
+}
+
+interface CoffeeProps {
+  coffees: CoffeeData[];
+}
+
+export function coffeesReducer(state: CoffeeProps, action: any) {
+  const { type, payload } = action;
+  switch (type) {
+    case ActionTypes.ADD_NEW_COFFEE:
+      return {
+        ...state,
+        coffees: [...state.coffees, payload.coffee],
+      };
+
+    case ActionTypes.REMOVE_COFFEE_CART:
+      return {
+        ...state,
+        coffees: state.coffees.filter((coffee) => coffee.id !== payload.id),
+      };
+
+    case ActionTypes.UPDATE_AMOUNT_COFFEE_IN_CART:
+      const coffeeIndex = state.coffees.findIndex((coffee) => {
+        return coffee.id === payload.id;
+      });
+
+      const tempCoffees = [...state.coffees];
+
+      tempCoffees[coffeeIndex].amount = payload.amount;
+
+      return {
+        coffees: [...tempCoffees],
+      };
+  }
 }
